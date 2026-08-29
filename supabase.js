f71d122e9e68032acbbf80a2c43a8fcdaacbde96
ridhimdid-lab <ridhimdid@gmail.com>
2026-08-29 13:10:36 +0800

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://pwltdwywqawxpsxmgsjc.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_CXl9e7c1Iu5PdTR0jgPtvA_GxH7vt2-";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
